5670e9cb3f93b0fce6b7f812114023f968277b39
crodilla <cristinarodillahernandez@gmail.com>
2023-07-25 08:22:18 +0200

    commit

--- App/app.py
+++ App/app.py
@@ -9,22 +9,10 @@ import numpy as np
 os.chdir(os.path.dirname(__file__))
 
 app = Flask(__name__)
-# Load the configuration
-# app.config['DEBUG'] = True
 
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'csv', 'txt', 'xls', 'xlsx'}
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Route for the GitHub page
-#@app.route('/<convertidor: nombre_variable>', methods=['POST'])
-#def git_update():
-#    repo = git.Repo('./Genomes_project')
-#    origin = repo.remotes.origin
-#    repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-#    origin.pull()
-#    return "", 200
+# Load the model
+gen_model = pickle.load(open('genetic_model_def.pkl', 'rb'))
+sub_model = pickle.load(open('subclass_model_def.pkl', 'rb'))
 
 @app.route('/', methods=['GET'])
 def home():
@@ -34,40 +22,13 @@ def home():
 def params():
     return render_template('params.html')
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'choose_file' not in request.files:
-        flash('No file part')
-        # return to home page
-        return redirect(url_for('home'))
-    file = request.files['choose_file']
-
-    # If user does not select file, browser submits an empty file without filename
-    if file and allowed_file(file.filename):
-        data = pd.read_csv(file)
-        length = len(data)
-        gen_model = pickle.load(open('genetic_model_def.pickel', 'rb'))
-        sub_model = pickle.load(open('subclass_model_def.pickel', 'rb'))
-        gen_prediction = gen_model.predict(data)
-        sub_prediction = sub_model.predict(data)
-
-        if length > 1:
-            df = pd.DataFrame(data, columns=data.columns)
-            data = data.reshape(1, -1)
-            gen_prediction = gen_model.predict(data)
-            sub_prediction = sub_model.predict(data)
-            gen_result = df.inverse_transform(gen_prediction)
-            sub_result = df.inverse_transform(sub_prediction)
-            df.insert(0, 'Genetic_disorder', gen_result)
-            df.insert(0, 'Disorder_subclass', sub_result)
-            return render_template('predict_table.html', tables=[df.to_html(classes='data', header='True')])
-        
-        elif length == 1:
-            return render_template('predict.html', disorder = data.inverse_transform(gen_prediction),  subclass = data.inverse_transform(sub_prediction))
+    int_features = [float(x) for x in request.form.values()]
+    final_features = np.array(int_features).reshape(1, -1)
+    gen_prediction = gen_model.predict(final_features)
+    sub_prediction = sub_model.predict(final_features)
+    return render_template('index.html', prediction_text = f'Patient suffers from the disease group {gen_prediction}./n/n The specific condition is named "{sub_prediction}".')
 
 if __name__ == '__main__':
     app.run(debug=True)
