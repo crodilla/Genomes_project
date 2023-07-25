d51d6447bb91a256116356793581dea0504f5ecc
crodilla <cristinarodillahernandez@gmail.com>
2023-07-25 20:28:21 +0200

    commit

--- App/app.py
+++ App/app.py
@@ -5,12 +5,14 @@ import os
 import pickle
 import pandas as pd
 import numpy as np
+import joblib
 
 os.chdir(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 # Load the model
+encoder = joblib.load(open('encoder.pkl', 'rb'))
 gen_model = pickle.load(open('genetic_model_def.pkl', 'rb'))
 sub_model = pickle.load(open('subclass_model_def.pkl', 'rb'))
 
@@ -24,14 +26,16 @@ def params():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
+    int_features = [int(x) for x in request.form.values()]
     final_features = np.array(int_features).reshape(1, -1)
     gen_prediction = gen_model.predict(final_features)
     sub_prediction = sub_model.predict(final_features)
-    return render_template('index.html', prediction_text = f'Patient suffers from the disease group {gen_prediction}./n/n The specific condition is named "{sub_prediction}".')
+    pred1 = encoder.inverse_transform([int(gen_prediction)])
+    pred2 = encoder.inverse_transform([int(sub_prediction)])
+    return render_template('index.html', prediction_text = f'Patient suffers from the disease group {pred1[0]}. The specific condition is named "{pred2[0]}".')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=80)
  
 
         
